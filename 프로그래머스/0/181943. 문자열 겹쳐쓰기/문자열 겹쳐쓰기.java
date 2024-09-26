d61bb09c4ce2691d7c865ff70221526d73d40765
choihyeonji00 <151499430+choihyeonji00@users.noreply.github.com>
2024-09-27 00:49:24 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 2.03 ms, Memory: 79.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String solution(String my_string, String overwrite_string, int s) {
+        
+        String answer = "";
+        answer = my_string.substring(0, s);
+        answer += overwrite_string;
+        answer += my_string.substring(s + overwrite_string.length(), my_string.length());
+        return answer;
+        
+        
+    }
+}
