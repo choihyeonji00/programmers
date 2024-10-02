2e51e01813897ab92a916786777bc957c298921c
choihyeonji00 <151499430+choihyeonji00@users.noreply.github.com>
2024-10-02 13:04:46 +0900

    [level 0] Title: 카운트 업, Time: 0.05 ms, Memory: 76 MB -BaekjoonHub

---
+++ 프로그래머스/0/181920. 카운트 업/카운트 업.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public List solution(int start, int end) {
+        
+        List<Integer> answer = new ArrayList<>();
+        
+        for (int i = start; i <= end;i++) {
+            answer.add(i);
+        }
+        
+        return answer;
+    }
+}
