d5650ae14fa93cb7466860d22b09bd4379eee651
choihyeonji00 <151499430+choihyeonji00@users.noreply.github.com>
2024-10-22 19:47:29 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.12 ms, Memory: 76.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public String solution(String my_string) {
+        
+        StringBuffer str = new StringBuffer(my_string);
+        
+        return str.reverse().toString();
+    }
+}
