d39ce1a34cd1305b9f6e2bfa59276f312c7f41f4
choihyeonji00 <151499430+choihyeonji00@users.noreply.github.com>
2024-10-29 18:53:39 +0900

    [level 0] Title: 369게임, Time: 0.18 ms, Memory: 75.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120891. 369게임/369게임.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int order) {
+        int answer = 0;
+        String order_s=Integer.toString(order);
+        String arr[]=order_s.split("");
+        for(int i=0;i<arr.length;i++){
+            if(arr[i].equals("3") || arr[i].equals("6") || arr[i].equals("9")){
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
