c3782832a32bae1f4bb2b20611fcd099ded1a850
choihyeonji00 <151499430+choihyeonji00@users.noreply.github.com>
2024-12-15 22:38:33 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.08 ms, Memory: 86.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -0,0 +1,13 @@
+class Solution {
+  public boolean solution(String s) {
+      if(s.length() == 4 || s.length() == 6){
+          try{
+              int x = Integer.parseInt(s);
+              return true;
+          } catch(NumberFormatException e){
+              return false;
+          }
+      }
+      else return false;
+  }
+}
