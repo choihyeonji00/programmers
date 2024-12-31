08fc7001153376008bb3839f6dd3ff971f45beac
choihyeonji00 <151499430+choihyeonji00@users.noreply.github.com>
2025-01-01 03:10:23 +0900

    [level 1] Title: [1차] 비밀지도, Time: 2.38 ms, Memory: 73.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -0,0 +1,16 @@
+class Solution {
+  public String[] solution(int n, int[] arr1, int[] arr2) {
+      String[] answer = new String[n];
+      String temp;
+
+      for(int i = 0 ; i < n ; i++){
+          temp = String.format("%16s", Integer.toBinaryString(arr1[i] | arr2[i]));
+          temp = temp.substring(temp.length() - n);
+          temp = temp.replaceAll("1", "#");
+          temp = temp.replaceAll("0", " ");
+          answer[i] = temp;
+      }
+
+      return answer;
+  }
+}
